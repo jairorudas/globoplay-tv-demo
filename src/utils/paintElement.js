603db775c5fa8e6964a713c8ab8a737ac624de6d
jairorudas <jairorudasp@gmail.com>
2020-09-27 21:40:03 -0300

    utility to paint keys

---
+++ src/utils/paintElement.js
@@ -0,0 +1,9 @@
+export default (id) => {
+    const element = document.getElementById(id)
+    element.style.background = '#E50914'
+}
+
+export const removeFocus = (id, color) => {
+    const element = document.getElementById(id)
+    element.style.background = color
+}
