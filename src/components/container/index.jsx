ba5199c69fff3986fca414d6f22a8f5b343c1ff7
jairorudas <jairorudasp@gmail.com>
2020-09-27 21:38:52 -0300

    carousel component wotking

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AplicationContext } from "../../context/aplicationContext";
+import './styles.css'
 
 const Item = ({ filme }) => (
   <img className="img-filme" src={filme.show.image?.medium} alt="img" />
@@ -8,55 +9,85 @@ const Item = ({ filme }) => (
 export default ({ id }) => {
   const {
     filmes,
-    currentFilme,
-    currentContainer,
-    setCurrentFilme,
-    getFilmes,
   } = useContext(AplicationContext);
-  const UL = useRef(null);
-  // const [lastItem, setLastItem] = useState(0);
-  const containerID = id;
-  let i = 0;
 
+  const [currentNav, setCurrentNav] = useState()
+  let index = 0;
+
+  const animateCarousel = (side) => {
+    let NEW_TRANSLATE = null
+    const NODE = document.getElementById(`nav-${id}`);
+    const LIST = NODE.getElementsByTagName("li");
+    const DATA_TRANSFORM = LIST[0].style.transform
+    const value =  (+DATA_TRANSFORM?.slice(DATA_TRANSFORM.length-6, DATA_TRANSFORM.length - 3)) || 90   
+    
+    if(side === 'prev') {
+      NEW_TRANSLATE = value - 110
+      
+    for (let item of LIST) {
+      item.style.transform = `translate(calc(210px - ${NEW_TRANSLATE}%))`;
+    }
+  }else {
+      NEW_TRANSLATE = value + 110
+      for (let item of LIST) {
+        item.style.transform = `translate(calc(210px - ${NEW_TRANSLATE}%))`;
+      }
+    }
+  }
+  
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
-      if (currentContainer === containerID && e.key === "ArrowLeft") {
-        goToFilme("prev");
-      }
-
-      if (currentContainer === containerID && e.key === "ArrowRight") {
-        goToFilme("next");
+      console.log(localStorage.getItem('currentNav'), 'current nav')
+      let currentContainer = +(localStorage.getItem('currentNav'))
+      setCurrentNav(currentContainer)
+      if(currentContainer === id) {
+        if (e.key === "ArrowLeft") {
+          goToFilme("prev");
+        }
+  
+        if (e.key === "ArrowRight") {
+          goToFilme("next");
+        }
+        
+        if (e.key === "ArrowDown") {
+          console.log('down');
+        }
       }
     });
-  }, [filmes]);
+  },[]);
 
-  // useEffect(() => {
-  //   console.log(filmes, "FILMES");
-  //   console.log(getFilmes(), "GET FILMES");
-  //   console.log(UL.current, "UL");
-  // }, [filmes]);
+ 
+
+ 
+  // let filme = filmes.find((filme) => filme.show.id === +li.id);
 
   const goToFilme = (move) => {
-    const newPosition = move === "next" ? (i += 1) : (i -= 1);
-    if (i >= 0) {
-      let li = UL.current.getElementsByTagName("li")[newPosition];
-      let filme = filmes.find((filme) => filme.show.id === +li.id);
-      setCurrentFilme(filme);
-      console.log(currentFilme, "New current Filme");
+    if(move === 'next') {
+      index +=  1
+      animateCarousel(move)
+    } else {
+      if(index === 0) {
+        localStorage.setItem('currentNav', 1)
+      } else {
+        index -= 1
+        animateCarousel(move)
+      }
     }
   };
-
-  return (
-    <ul className="horizontal" id={`container-${id}`} ref={UL}>
+  
+  return (<section style={{position: "relative"}}>
+    <ul className="horizontal" id={`nav-${id}`}>
       {filmes.map((el, i) => (
         <li
           key={el.show.id}
-          className={`list ${true ? "active" : ""}`}
+          className={`list`}
           id={el.show.id}
         >
           <Item filme={el} />
         </li>
       ))}
     </ul>
+      <div className={` ${currentNav === id ? 'selection' : ''} `}></div>
+    </section>
   );
 };
