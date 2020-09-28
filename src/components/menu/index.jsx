a0bdf11b744721cd005badfcf2eca231237cdd04
jairorudas <jairorudasp@gmail.com>
2020-09-27 21:39:45 -0300

    menu component working finished

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import search from "../../assets/search.png";
 import feed from "../../assets/feed.png";
 import market from "../../assets/market.png";
@@ -7,23 +7,50 @@ import user from "../../assets/user.png";
 import "./styles.css";
 
 export default () => {
+  const [currentNav, setCurrentNav] = useState()
+
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      const id = setTimeout(() => {
+        const currentContainer = +localStorage.getItem('currentNav');
+        setCurrentNav(currentContainer)
+        clearTimeout(id)
+      }, 100)
+
+      if(e.key === 'ArrowRight') {
+        localStorage.setItem('currentNav', 1)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(currentNav)
+  }, [currentNav])
+
   return (
-    <ul className="fatherList">
-      <li className="item">
-        <img src={search} alt="search" />
-      </li>
-      <li className="item">
-        <img src={feed} alt="search" />
-      </li>
-      <li className="item">
-        <img src={market} alt="search" />
-      </li>
-      <li className="item">
-        <img src={videoPlayer} alt="search" />
-      </li>
-      <li className="item">
-        <img src={user} alt="search" />
-      </li>
-    </ul>
+    <section id="nav-0">
+      <ul className={`fatherList ${currentNav === 0 ? 'menu-open' : ''}`} id="menu">
+        <li className={`item ${currentNav === 0 ? 'active' : ''}`} >
+          <img src={search} alt="search" />
+          <p className={`${currentNav === 0 ? 'show-label' : ''}`}> Busca</p>
+        </li>
+        <li className="item">
+          <img src={market} alt="Inicio" />
+          <p className={`${currentNav === 0 ? 'show-label' : ''}`}>Inicio</p>
+        </li>
+        <li className="item">
+          <img src={feed} alt="Now" />
+          <p className={`${currentNav === 0 ? 'show-label' : ''}`}>Agora na TV</p>
+        </li>
+        <li className="item">
+          <img src={videoPlayer} alt="Categoria" />
+          <p className={`${currentNav === 0 ? 'show-label' : ''}`}>Categorias</p>
+        </li>
+        <li className="item">
+          <img src={user} alt="User" />
+          <p className={`${currentNav === 0 ? 'show-label' : ''}`}>User</p>
+        </li>
+      </ul>
+    </section>
   );
 };
