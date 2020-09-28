0196f38af2e8a5cdc9a95ce0d6721349c40d67e5
jairorudas <jairorudasp@gmail.com>
2020-09-27 21:40:45 -0300

    Aplication context created

--- src/context/aplicationContext.jsx
+++ src/context/aplicationContext.jsx
@@ -3,45 +3,46 @@ import React, { useState, createContext, useEffect } from "react";
 export const AplicationContext = createContext();
 
 const AplicationProvider = ({ children }) => {
-  const [currentContainer, setCurrentContainer] = useState(0);
   const [currentFilme, setCurrentFilme] = useState(null);
-  // const [totalVerticalItems, setTotalVerticalItems] = useState();
   const [filmes, setFilmes] = useState([]);
-
-  useEffect(() => {
-    focusCurrentFilme();
-    console.log(currentFilme, "filme");
-  }, [currentFilme]);
-
-  const focusCurrentFilme = () => {
-    if (currentFilme?.show?.id) {
-      let node = document.getElementById(currentFilme.show.id);
-      node.style.borderColor = "#ff0000cc";
-      node.style.borderWidth = "2px";
-      node.style.borderStyle = "solid";
-      node.focus();
-    }
-  };
-
-  const getFilmes = () => filmes;
+  const [nameFilme, setNameFilme] = useState('')
 
   useEffect(() => {
     fetch("http://api.tvmaze.com/search/shows?q=girls").then(async (res) => {
       const filmes = await res.json();
+      console.log(filmes)
       setFilmes(filmes);
-      setCurrentFilme(filmes[0]);
     });
   }, []);
+
+  const getMainFilmes = () => {
+    if (nameFilme) {
+     filmes.filter( filme => {
+      const name = filme.show.name
+      
+      if((name.toLowerCase()).includes(nameFilme)){
+        return filme
+      }
+      
+     })
+    } else {
+      return filmes
+    }
+  }
+
+  useEffect(() => {
+    console.log(nameFilme, 'Name Filme')
+  }, [nameFilme])
 
   return (
     <AplicationContext.Provider
       value={{
         filmes,
+        nameFilme,
         currentFilme,
-        currentContainer,
-        getFilmes,
         setCurrentFilme,
-        setCurrentContainer,
+        setNameFilme,
+        getMainFilmes
       }}
     >
       {children}
